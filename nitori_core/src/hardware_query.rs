9d2efff7339a04ef2cf071a3c9eaaddac213df6d
H2Sxxa <h2sxxa0w0@gmail.com>
2023-11-15 17:45:22 +0800

    ⚡️ Optmize loop

--- nitori_core/src/hardware_query.rs
+++ nitori_core/src/hardware_query.rs
@@ -9,13 +9,13 @@ pub fn query(target: &'static str) -> String {
     let con = COMLibrary::new().unwrap();
     let wmi = WMIConnection::new(con).unwrap();
     let mut data: DATAMAP = HashMap::new();
-    for t in serde_json::from_str::<QueryArg>(target)
+    serde_json::from_str::<QueryArg>(target)
         .unwrap()
         .target
         .iter()
-    {
-        data.insert(t, wmi.raw_query(format!("SELECT * FROM {}", t)).unwrap());
-    }
+        .for_each(|t| {
+            data.insert(t, wmi.raw_query(format!("SELECT * FROM {}", t)).unwrap());
+        });
 
     let mut result: QueryResult = HashMap::new();
     result.insert("data", data);
